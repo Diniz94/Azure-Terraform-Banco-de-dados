e612445a684a872f4eca9f9d5637dbf3969fc31f
Diniz94 <claudio.diniz.antunes@hotmail.com>
2022-04-03 20:33:12 -0300

    tf-database

--- AZSQLDATABASE/sqldatabase.tf
+++ AZSQLDATABASE/sqldatabase.tf
@@ -3,17 +3,24 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "rg-sqldatabase"
+  name     = "rg-sqldatabase"
   location = "brazilsouth"
 }
 
 resource "azurerm_sql_server" "sqlserver" {
-  name = "sqlservertf"
+  name                = "sqlservertf-azure"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
 
-  version = "12.0"
-  administrator_login = "administrador"
+  version                      = "12.0"
+  administrator_login          = "administrador"
   administrator_login_password = "Claudio@2022"
 }
 
+
+resource "azurerm_mssql_database" "mssqdatabase" {
+  name      = "db_terraform"
+  server_id = azurerm_sql_server.sqlserver.id
+  collation = "SQL_latin1_General_CP1_CI_AS"
+  sku_name  = "Basic"
+}
